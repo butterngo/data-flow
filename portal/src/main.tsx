47c5a4c5c2c34b272075f1d49020f0c433b3734d
thuongbui <buianhthuong122@gmail.com>
2023-06-16 15:18:13 +0700

    Implement React query, axios and remove unnessarry code

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -4,11 +4,26 @@ import App from "./App.tsx";
 import "./index.css";
 import "reactflow/dist/style.css";
 import { ReactFlowProvider } from "reactflow";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ReactFlowProvider>
-      <App />
-    </ReactFlowProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactFlowProvider>
+        <App />
+      </ReactFlowProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
